6b0fcdf64c0a4fa1bb885554f9a3866f8af99a0d
shadowbynl <nlby3306@163.com>
2021-07-19 14:28:16 +0800

    配置完成vscode集成c++的环境

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        Sleep(2000);
+        cout << "NihOn" << endl;
+    }
+    system("pause");
+    return 0;
+}
